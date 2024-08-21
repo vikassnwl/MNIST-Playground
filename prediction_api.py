fd626fac13def6be0b480011896087cee4eb62d5
Vikas Sanwal <vikassnwl@gmail.com>
2024-08-21 21:50:31 +0530

    Implement center_digit function

--- prediction_api.py
+++ prediction_api.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
-from utils import data_url_to_numpy_array
-from PIL import Image
+import cv2
+from utils import center_digit
 
 
 app = Flask(__name__)
@@ -12,9 +12,7 @@ CORS(app)
 def load_model():
     print("Loading the mnist model...")
     global model
-    # model = tf.keras.models.load_model("mnist_model")
-    # model = tf.keras.models.load_model("mnist-model.h5")
-    model = tf.keras.models.load_model("mnist_model_cnn")
+    model = tf.keras.models.load_model("model/mnist_model_cnn")
     print("mnist model loaded successfully!")
 
 load_model()
@@ -23,16 +21,11 @@ load_model()
 def hello_world():
     data = request.json
     image = np.array(data["pixels"], dtype=np.uint8)
-    # print(data["pixelMatrix"])
-    # print(data["dataURL"])
-    # image = data_url_to_numpy_array(data["dataURL"], mode="binary")
-    img = Image.fromarray(image)
-    img.save("image.jpg")
-    print("================== Model Prediction =====================")
-    print(f"image_shape={image.shape}")
-    print(model.predict(np.array([image.flatten()])).argmax(axis=1))
-    probas = model.predict(np.array([image.flatten()])/255.)
-    print(probas)
+    cv2.imwrite("images/image.jpg", image)
+    centered_image = center_digit(image)
+    cv2.imwrite("images/centered_image.jpg", centered_image)
+    centered_image /= 255.
+    probas = model.predict(np.array([centered_image]))
     max_proba = probas.max(axis=1)[0]
     prediction = probas.argmax(axis=1)[0]
     return jsonify({"prediction": str(prediction), "proba": str(max_proba)})
--- utils.py
+++ utils.py
@@ -2,33 +2,72 @@ import base64
 import numpy as np
 from PIL import Image
 import io
+import cv2
+
 
 def data_url_to_numpy_array(data_url, mode="gray"):
-  """Converts a data URL to a NumPy array.
+    """Converts a data URL to a NumPy array.
 
-  Args:
-    data_url: The data URL to convert.
+    Args:
+      data_url: The data URL to convert.
 
-  Returns:
-    A NumPy array representing the image data.
-  """
+    Returns:
+      A NumPy array representing the image data.
+    """
 
-  # Extract the base64-encoded data
-  img_str = data_url.split(',')[1]
-  img_data = base64.b64decode(img_str)
+    # Extract the base64-encoded data
+    img_str = data_url.split(",")[1]
+    img_data = base64.b64decode(img_str)
 
-  # Create a binary stream from the data
-  image_file = io.BytesIO(img_data)
+    # Create a binary stream from the data
+    image_file = io.BytesIO(img_data)
 
-  # Load the image using PIL
-  img = Image.open(image_file)
+    # Load the image using PIL
+    img = Image.open(image_file)
 
-  # Convert the image to a NumPy array
-  if mode == "gray":
-    img_array = np.array(img.convert("L"))
-  elif mode == "binary":
-    img_array = 255-np.array(img.convert("L"))
-  else:
-    img_array = np.array(img)
+    # Convert the image to a NumPy array
+    if mode == "gray":
+        img_array = np.array(img.convert("L"))
+    elif mode == "binary":
+        img_array = 255 - np.array(img.convert("L"))
+    else:
+        img_array = np.array(img)
 
-  return img_array
+    return img_array
+
+
+def capture_all_cntrs(cntrs):
+    min_x, min_y, max_x, max_y = 1e9, 1e9, 0, 0
+    for cntr in cntrs:
+        x, y, w, h = cv2.boundingRect(cntr)
+        min_x = min(x, min_x)
+        min_y = min(y, min_y)
+        max_x = max(max_x, x + w)
+        max_y = max(max_y, y + h)
+
+    return min_x, min_y, max_x - min_x, max_y - min_y
+
+
+def center_digit(gray_image):
+    _, gray_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(
+        gray_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    x, y, w, h = capture_all_cntrs(contours)
+    if w > h:
+        resize_w = 20
+        resize_h = h * 20 // w
+    else:
+        resize_w = w * 20 // h
+        resize_h = 20
+    cropped_image = gray_image[y : y + h, x : x + w]
+    resized_image = cv2.resize(cropped_image, (resize_w, resize_h))
+    gray_h, gray_w = gray_image.shape
+    resized_top = (gray_h - resize_h) // 2
+    resized_bottom = resized_top + resize_h
+    resized_left = (gray_w - resize_w) // 2
+    resized_right = resized_left + resize_w
+    canvas = np.zeros(gray_image.shape)
+    canvas[resized_top:resized_bottom, resized_left:resized_right] = resized_image
+
+    return canvas
